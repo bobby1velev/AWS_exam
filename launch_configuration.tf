a5a02dfa5bef885679a91cd6fe5f82780b223be4
borislav velev <borislav.velev.study@abv.bg>
2023-06-08 10:37:37 +0300

    Adjusting before first test deployment

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -1,4 +1,5 @@
-# Create a Launch Configuration -----------------------------------------------
+# Launch template
+
 resource "aws_launch_template" "machine_image_type" {
   name_prefix            = "terraform"
   image_id               = var.used_image
@@ -22,20 +23,26 @@ resource "aws_launch_template" "machine_image_type" {
   )
 }
 
-# Create a ASG ----------------------------------------------------------------
+############################################
+# Auto scaling group files
+############################################
+
+# Auto scaling group creation
+
 resource "aws_autoscaling_group" "my_autoscaling_group" {
   desired_capacity   = 2
   max_size           = 4
   min_size           = 2
   vpc_zone_identifier = [aws_subnet.subnet_3.id, aws_subnet.subnet_4.id]
-target_group_arns = [ aws_lb_target_group.alb-target.arn ]
+  target_group_arns = [ aws_lb_target_group.alb-target.arn ]
   launch_template {
     id      = aws_launch_template.machine_image_type.id
     version = "$Latest"
   }
 }
 
-# Create Auto Scale Policy ----------------------------------------------------
+# Auto Scaling Policy
+
 resource "aws_autoscaling_policy" "the_policy" {
   name                   = "autoscaling_policy"
   scaling_adjustment     = 1
@@ -43,7 +50,9 @@ resource "aws_autoscaling_policy" "the_policy" {
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.my_autoscaling_group.name
 }
-# Cloudwatch config -----------------------------------------------------------
+
+# Cloudwatch alarm settings
+
 resource "aws_cloudwatch_metric_alarm" "my_alarm" {
   alarm_name                = "terraform-test-alarm"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
@@ -53,14 +62,15 @@ resource "aws_cloudwatch_metric_alarm" "my_alarm" {
   period                    = 60
   statistic                 = "Average"
   threshold                 = 80
-  alarm_description         = "Cpu utilization threshold overload"
+  alarm_description         = "Cpu utilization threshold overload - adding another instance"
   alarm_actions = [aws_autoscaling_policy.the_policy.arn]
   dimensions = {
     "AutoScalingGroupName" = aws_autoscaling_group.my_autoscaling_group.name
   }
 }
 
-# Attach Policy ---------------------------------------------------------------
+# Policy attachment for auto scaling group
+
 resource "aws_autoscaling_attachment" "asg_attachment_lb" {
   autoscaling_group_name = aws_autoscaling_group.my_autoscaling_group.id
   lb_target_group_arn = aws_lb_target_group.alb-target.arn
